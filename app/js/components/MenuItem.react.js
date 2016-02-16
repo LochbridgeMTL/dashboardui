26910557f142ff4b20686ceaf85e0fd03473ddc1
Fred Leclerc <fred.leclerc@gmail.com>
2016-02-16 14:16:24 -0500

    New layout

--- app/js/components/MenuItem.react.js
+++ app/js/components/MenuItem.react.js
@@ -8,16 +8,20 @@ var MenuItem = React.createClass({
     return {active: false}
   },
 
+  toggle: function() {
+    this.setState({active: !(this.state.active)});
+  },
+
   render: function() {
 
     var className = (this.state.active ? "menu-item menu-item-active" : "menu-item");
-
+    var contextualClass = (this.state.active ? "contextual-items" : "contextual-items-hidden");
     return (
       <div>
-        <div className={className}>
+        <div className={className} onClick={this.toggle}>
           {this.props.label}
         </div>
-        <div className="contextual-items">
+        <div className={contextualClass}>
           <div><a href="#">Complement</a></div>
           <div><a href="#">Complement</a></div>
           <div><a href="#">Complement</a></div>
